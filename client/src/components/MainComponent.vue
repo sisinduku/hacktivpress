5a7ddd61e91f2ec4f321488c429de3fbb2e1bb1a
sisinduku <saptanto.sindu@gmail.com>
2017-11-09 14:34:09 +0700

    Add Client Init

---
+++ client/src/components/MainComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-container id='MainComponent'>
+    <b-row>
+      <b-col>
+        <b-navbar toggleable="md" type="dark" variant="info">
+          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+          <b-navbar-brand href="#">hactivpress</b-navbar-brand>
+          <b-collapse is-nav id="nav_collapse">
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item text="Lang" right>
+                <b-btn size="sm" variant="primary">
+                  Login with Facebook
+                </b-btn>
+              </b-nav-item>
+          </b-collapse>
+        </b-navbar>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+
+export default {
+  name: 'MainComponent',
+
+  data: () => ({}),
+
+  created() {
+    (function (d, s, id) {
+      var js
+      var fjs = d.getElementsByTagName(s)['0']
+      if (d.getElementById(id)) return
+      js = d.createElement(s)
+      js.id = id
+      js.src = '//connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    }(document, 'script', 'facebook-jssdk'))
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: process.env.FBAPPID,
+        cookie: true, // enable cookies to allow the server to access
+        xfbml: true, // parse social plugins on this page
+        version: 'v2.8' // use graph api version 2.8
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import store from './vuex/store'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://localhost:3000/'
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
